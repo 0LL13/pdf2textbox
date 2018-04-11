bf64d252b7f3ce45a969cff3fe2bea80174146fd
Oliver <hardy.ecc95@gmail.com>
2018-04-11 14:36:21 +0200

    Version 0.1

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils import setup
+
+
+setup(
+    name = 'pdf2textbox',
+    packages = ['pdf2textbox'],
+    version = '0.1'
+    description = 'A PDF-to-text converter based on pdfminer2'
+    author = 'Oliver Stapel'
+    author_email = 'hardy.ecc95@gmail.com'
+    url = 'https://github.com/0LL13/pdf2textbox',
+    download_url = 'https://github.com/0LL13/pdf2textbox/archive/0.1.tar.gz',
+    keywords = ['PDF', 'pdfminer2', 'PDF conversion']
+    classifiers = []
+    )
